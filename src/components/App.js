69c660e18ddaeef238cce3e853a30a643d4594e9
DhanushM12 <dhanushmukhi@gmail.com>
2020-11-07 12:12:33 +0530

    added the header and moviecard components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import Header from './Header';
+import MovieCard from './MovieCard';
 function App() {
-  return <div className="App">Hello My OMDB Api</div>;
+  return (
+    <div className="App">
+      Hello My OMDB Api
+      <Header />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+        <div className="list"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

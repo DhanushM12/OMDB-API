db92cd4212b9cfabeab0dd168d451061cf94f722
DhanushM12 <dhanushmukhi@gmail.com>
2020-11-07 19:11:58 +0530

    added the applyMiddleware of redux to configure store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  //logger code
+  console.log('ACTION_TYPE = ', action.type);
+  next(action);
+};
+const store = createStore(rootReducer, applyMiddleware(logger));
+console.log('state', store);
 
 ReactDOM.render(<App store={store} />, document.getElementById('root'));
